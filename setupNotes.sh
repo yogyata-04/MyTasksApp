0177056d6821bcdf106c5a7e99c190f19b8ceae1
Yogyata Singh <134354066+yogyata-04@users.noreply.github.com>
2026-02-05 20:55:07 +0530

    Update setupNotes.sh

--- setupNotes.sh
+++ setupNotes.sh
@@ -3,3 +3,6 @@ npm init -y (creates package.json file)
 npm install express@version (creates package-lock.json and node_modules)
 
 create server.js / app.js / or whatever
+
+
+node server.js (to run server.js)
